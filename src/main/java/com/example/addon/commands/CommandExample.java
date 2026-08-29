648c543e8966fb40debb09caf13e184701c05b92
erayyyymisim <ugurkaan3434@gmail.com>
2026-08-29 19:22:47 +0300

    Update CommandExample.java

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -1,31 +1,25 @@
 package com.example.addon.commands;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import meteordevelopment.meteorclient.commands.Command;
-import net.minecraft.client.multiplayer.ClientSuggestionProvider;
+import net.minecraft.commands.SharedSuggestionProvider;
 
-/**
- * The Meteor Client command API uses the <a href="https://github.com/Mojang/brigadier">same command system as Minecraft does</a>.
- */
 public class CommandExample extends Command {
-    /**
-     * The {@code name} parameter should be in kebab-case.
-     */
     public CommandExample() {
-        super("example", "Sends a message.");
+        super("example", "Sends a message to chat.", "ex");
     }
 
     @Override
-    public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder) {
-builder.executes(context -> {
-            info("hi");
+    public void build(LiteralArgumentBuilder<SharedSuggestionProvider> builder) {
+        builder.executes(context -> {
+            info("Hello from custom Meteor command!");
             return SINGLE_SUCCESS;
         });
 
         builder.then(literal("name").then(argument("nameArgument", StringArgumentType.word()).executes(context -> {
-            String argument = StringArgumentType.getString(context, "nameArgument");
-            info("hi, " + argument);
+            String name = StringArgumentType.getString(context, "nameArgument");
+            info("Hello, " + name + "!");
             return SINGLE_SUCCESS;
         })));
     }
