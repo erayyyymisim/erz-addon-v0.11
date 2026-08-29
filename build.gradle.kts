799ceef9822e814d9934550e3f5c1d2a45196c5f
erayyyymisim <ugurkaan3434@gmail.com>
2026-08-29 19:49:31 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ tasks.withType<JavaCompile>().configureEach {
 
 java {
     withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
